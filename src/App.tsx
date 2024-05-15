0c3412ca9083fafed3eafdc06f84025e22ed26ab
Abhishek Sharma <abhishek79009@gmail.com>
2024-05-15 12:03:59 +0530

    router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
 function App() {
   return (
     <>
-      <div className="text-red-500">helllo</div>
+    <Routes>
+      <Route path="/"  element={<Home/>}/>
+      <Route path="/about"  element={<About/>}/>
+    </Routes>
     </>
   );
 }
